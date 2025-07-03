4bf3d43301179fc0a6218355fc353d820d6adbb2
Tim <timothee.barran@gmail.com>
2025-07-03 14:43:19 +0200

    clean

--- database.py
+++ database.py
@@ -9,10 +9,6 @@ sqlite_url = os.getenv("DATABASE_URL", "sqlite:///database.db")
 
 engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
-@event.listens_for(engine, "connect")
-def _enable_sqlite_fk(dbapi_connection, _):
-    dbapi_connection.execute("PRAGMA foreign_keys = ON")
-
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
